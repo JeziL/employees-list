ba5a6082fff25e15d8c4a096f1a13ca700696b99
JeziL <wangjinlithu@gmail.com>
2025-04-16 16:02:17 +0800

    Subscribe data from stat view.

--- src/Statistics.jsx
+++ src/Statistics.jsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
 import ListAltIcon from '@material-ui/icons/ListAlt';
 import Emitter from './event';
 
-const Statistics = () => (
-  <div>
-    <Button variant="outlined" color="primary" onClick={() => { Emitter.emit('switch-table'); }}>
-      <ListAltIcon />
-      &ensp;切换至列表视图
-    </Button>
-  </div>
-);
+const Statistics = () => {
+  let eventListener;
+  const [members, setMembers] = useState([]);
+
+  useEffect(() => {
+    eventListener = Emitter.addListener('employee-data', (employees) => { setMembers(employees); });
+    return () => {
+      if (typeof eventListener === 'function') {
+        Emitter.removeListener(eventListener);
+      }
+    };
+  }, []);
+
+  return (
+    <div>
+      <Button variant="outlined" color="primary" onClick={() => { Emitter.emit('switch-table'); }}>
+        <ListAltIcon />
+        &ensp;切换至列表视图
+      </Button>
+      <p>
+        总人数：
+        {members.length}
+      </p>
+    </div>
+  );
+};
 
 export default Statistics;
