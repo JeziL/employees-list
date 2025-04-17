93df8e11f0f2ae594f4feaa6fc346268014be529
JeziL <wangjinlithu@gmail.com>
2025-04-17 18:28:39 +0800

    Add statistics view.

--- src/Statistics.jsx
+++ src/Statistics.jsx
@@ -1,14 +1,39 @@
+/* eslint-disable global-require */
+/* eslint-disable react/prop-types */
 import React, { useEffect, useState } from 'react';
 import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import * as echarts from 'echarts';
+import ReactECharts from 'echarts-for-react';
 import ListAltIcon from '@material-ui/icons/ListAlt';
+import Typography from '@material-ui/core/Typography';
+import {
+  getAddressInfo, getAgeInfo, getSexRatio, getDepartmentInfo,
+  getNameInfo,
+} from './utils';
 import Emitter from './event';
 
 const Statistics = () => {
   let eventListener;
   const [members, setMembers] = useState([]);
+  const [depList, setDepList] = useState([]);
+  const [mapLoaded, setMapLoaded] = useState(false);
+
+  const config = process.env.REACT_APP_CI === 'github_ci' ? JSON.parse(process.env.REACT_APP_PROJ_CONFIG) : require('./config.json');
+
+  const loadMap = () => {
+    fetch(config.dataSource.map)
+      .then((response) => response.json())
+      .then((mapObj) => {
+        echarts.registerMap('China-P', mapObj);
+        setMapLoaded(true);
+      });
+  };
 
   useEffect(() => {
+    loadMap();
     eventListener = Emitter.addListener('employee-data', (employees) => { setMembers(employees); });
+    eventListener = Emitter.addListener('departmentList', (deps) => { setDepList(deps); });
     return () => {
       if (typeof eventListener === 'function') {
         Emitter.removeListener(eventListener);
@@ -16,18 +41,294 @@ const Statistics = () => {
     };
   }, []);
 
+  const depInfo = getDepartmentInfo(members, depList);
+  const sexInfo = getSexRatio(members, depList);
+  const ageInfo = getAgeInfo(members);
+  const addrInfo = getAddressInfo(members);
+  const nameInfo = getNameInfo(members);
+
   return (
-    <div>
-      <Button variant="outlined" color="primary" onClick={() => { Emitter.emit('switch-table'); }}>
+    <div style={{ marginTop: 48 }}>
+      <Button
+        variant="outlined"
+        color="primary"
+        style={{ marginBottom: 24 }}
+        onClick={() => { Emitter.emit('switch-table'); }}
+      >
         <ListAltIcon />
         &ensp;切换至列表视图
       </Button>
-      <p>
-        总人数：
-        {members.length}
-      </p>
+      <Stat title="数据总人数" value={members.length} comma />
+      <Divider style={{ marginBottom: 24 }} />
+      <Typography variant="h4" gutterBottom>
+        <b>部门分布</b>
+      </Typography>
+      <ReactECharts option={{
+        tooltip: {
+          trigger: 'item',
+        },
+        legend: {
+          show: false,
+          orient: 'vertical',
+          left: 'right',
+        },
+        series: [
+          {
+            name: '部门',
+            type: 'pie',
+            radius: '80%',
+            data: depInfo,
+            center: ['50%', '50%'],
+            label: { show: false },
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)',
+              },
+            },
+          },
+        ],
+      }}
+      />
+      <Divider style={{ marginBottom: 24 }} />
+      <Typography variant="h4" gutterBottom>
+        <b>性别分布</b>
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        综合男女比&nbsp;
+        {sexInfo.allRatio.toFixed(2)}
+        :1，最高部门
+        {sexInfo.maxDep}
+        （
+        {sexInfo.maxRatio.toFixed(2)}
+        :1），最低部门
+        {sexInfo.minDep}
+        （
+        {sexInfo.minRatio.toFixed(2)}
+        :1）。
+      </Typography>
+      <ReactECharts option={{
+        tooltip: {
+          trigger: 'item',
+        },
+        legend: {
+          show: false,
+          orient: 'vertical',
+          left: 'right',
+        },
+        series: [
+          {
+            name: '性别',
+            type: 'pie',
+            radius: '80%',
+            data: [
+              { name: '男', value: members.filter((m) => m.sex === '1').length },
+              { name: '女', value: members.filter((m) => m.sex === '0').length },
+            ],
+            center: ['50%', '50%'],
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)',
+              },
+            },
+          },
+        ],
+      }}
+      />
+      <ReactECharts option={{
+        title: {
+          text: '各部门男女比',
+          left: 'center',
+        },
+        xAxis: {
+          type: 'category',
+          data: sexInfo.data.map((d) => d.name),
+          axisTick: { show: false },
+          axisLabel: { show: false },
+        },
+        yAxis: {
+          type: 'value',
+          min: 0,
+          max: 12,
+        },
+        series: [{
+          data: sexInfo.data.map((d) => d.value),
+          type: 'bar',
+        }],
+        tooltip: {
+          trigger: 'axis',
+          axisPointer: {
+            type: 'shadow',
+          },
+        },
+      }}
+      />
+      <Divider style={{ marginBottom: 24 }} />
+      <Typography variant="h4" gutterBottom>
+        <b>年龄分布</b>
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        总体平均年龄&nbsp;
+        {ageInfo.average}
+        &nbsp;岁，最年长的是
+        {ageInfo.maxName}
+        （
+        {ageInfo.max}
+        岁），最年轻的是
+        {ageInfo.minName}
+        （
+        {ageInfo.min}
+        岁）。
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        出生于&nbsp;
+        {ageInfo.maxMonthDay}
+        &nbsp;的人数最多，有&nbsp;
+        {ageInfo.maxMonthDayCount}
+        &nbsp;人。
+      </Typography>
+      <ReactECharts option={{
+        title: {
+          text: '年龄区间分布',
+          left: 'center',
+        },
+        xAxis: [{ type: 'value' }],
+        yAxis: [{ type: 'value' }],
+        series: [{
+          name: '人数',
+          type: 'bar',
+          data: ageInfo.hist,
+          barWidth: '99.3%',
+          encode: {
+            x: 0,
+            y: 1,
+            tooltip: [2, 3, 1],
+          },
+        }],
+        tooltip: {
+          trigger: 'item',
+          axisPointer: {
+            type: 'shadow',
+          },
+          formatter: (params) => {
+            const v = params.data;
+            const n = params.seriesName;
+            return `${n}<br />${v[2]} - ${v[3]}\t<b>${v[1]}</b>`;
+          },
+        },
+      }}
+      />
+      <Divider style={{ marginBottom: 24 }} />
+      <Typography variant="h4" gutterBottom>
+        <b>地域分布</b>
+      </Typography>
+      {mapLoaded ? (
+        <ReactECharts option={{
+          tooltip: {
+            trigger: 'item',
+            showDelay: 0,
+            transitionDuration: 0.2,
+          },
+          visualMap: {
+            left: 'right',
+            min: 0,
+            max: 250,
+            inRange: {
+              color: [
+                '#313695',
+                '#4575b4',
+                '#74add1',
+                '#abd9e9',
+                '#e0f3f8',
+                '#ffffbf',
+                '#fee090',
+                '#fdae61',
+                '#f46d43',
+                '#d73027',
+                '#a50026',
+              ],
+            },
+            calculable: true,
+          },
+          series: [{
+            id: 'mapChart',
+            name: '人数',
+            type: 'map',
+            roam: true,
+            map: 'China-P',
+            emphasis: {
+              label: { show: false },
+            },
+            data: addrInfo,
+          }],
+        }}
+        />
+      ) : ('')}
+      <Divider style={{ marginBottom: 24 }} />
+      <Typography variant="h4" gutterBottom>
+        <b>姓名分布</b>
+      </Typography>
+      <Typography variant="body1" gutterBottom>
+        重名最多的是
+        {nameInfo.maxName}
+        ，有&nbsp;
+        {nameInfo.maxNameCount}
+        &nbsp;人。姓
+        {nameInfo.maxSurname}
+        的人最多，有&nbsp;
+        {nameInfo.maxSurnameCount}
+        &nbsp;人。
+      </Typography>
+      <ReactECharts option={{
+        title: {
+          text: '各姓氏人数',
+          left: 'center',
+        },
+        tooltip: {
+          trigger: 'item',
+        },
+        legend: {
+          show: false,
+          orient: 'vertical',
+          left: 'right',
+        },
+        series: [
+          {
+            name: '姓氏',
+            type: 'pie',
+            radius: '80%',
+            data: nameInfo.surnameData,
+            center: ['50%', '50%'],
+            label: { show: false },
+            emphasis: {
+              itemStyle: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: 'rgba(0, 0, 0, 0.5)',
+              },
+            },
+          },
+        ],
+      }}
+      />
     </div>
   );
 };
+
+const Stat = ({
+  title, value, precision = 0, comma = false,
+}) => (
+  <div>
+    <Typography variant="subtitle1" display="block" color="textSecondary" gutterBottom>
+      {title}
+    </Typography>
+    <Typography variant="h2" gutterBottom>
+      {comma ? value.toLocaleString('en-US', { minimumFractionDigits: precision }) : value.toFixed(precision)}
+    </Typography>
+  </div>
+);
 
 export default Statistics;
--- src/utils.js
+++ src/utils.js
@@ -84,3 +84,248 @@ export const downloadFile = (filename, content) => {
   element.setAttribute('download', filename);
   element.click();
 };
+
+export const getDepartmentInfo = (members, deps) => {
+  const data = [];
+  deps.forEach((dep) => {
+    data.push({
+      name: dep,
+      value: members.filter((m) => m.department === dep).length,
+    });
+  });
+  return data.sort((a, b) => b.value - a.value);
+};
+
+export const getSexRatio = (members, deps) => {
+  const data = [];
+  let femMin = 1e10;
+  let femMax = 0;
+  let femMinDep = '';
+  let femMaxDep = '';
+  deps.forEach((dep) => {
+    const total = members.filter((m) => m.department === dep);
+    const fem = total.filter((m) => m.sex === '0');
+    const femRatio = fem.length / (total.length - fem.length);
+    if (femRatio > femMax) {
+      femMax = femRatio;
+      femMaxDep = dep;
+    }
+    if (femRatio < femMin) {
+      femMin = femRatio;
+      femMinDep = dep;
+    }
+    data.push({
+      name: dep,
+      value: (1 / femRatio).toFixed(2),
+    });
+  });
+
+  return {
+    minRatio: 1 / femMax,
+    minDep: femMaxDep,
+    maxRatio: 1 / femMin,
+    maxDep: femMinDep,
+    allRatio: members.filter((m) => m.sex === '1').length / members.filter((m) => m.sex === '0').length,
+    data: data.sort((a, b) => b.value - a.value),
+  };
+};
+
+const getHistogramData = (numbers, binCount) => {
+  if (!Array.isArray(numbers) || numbers.length === 0) {
+    return [];
+  }
+
+  const min = Math.min(...numbers);
+  const max = Math.max(...numbers);
+  const binSize = (max - min) / binCount;
+
+  const bins = Array(binCount).fill(0);
+
+  numbers.forEach((num) => {
+    let index = Math.floor((num - min) / binSize);
+    if (index === binCount) index -= 1; // Edge case for max value
+    bins[index] += 1;
+  });
+
+  const histogram = bins.map((count, i) => {
+    const binStart = min + i * binSize;
+    const center = binStart + binSize / 2;
+    return [center, count, binStart.toFixed(1), (binStart + binSize).toFixed(1)];
+  });
+
+  return histogram;
+};
+
+export const getAgeInfo = (members) => {
+  let minAge = Infinity;
+  let maxAge = -Infinity;
+  let maxAgeName = '';
+  let minAgeName = '';
+  let sumAge = 0;
+  const ages = [];
+  const byMonthDay = {};
+  members.forEach((m) => {
+    if (m.age > 0) {
+      if (m.age > maxAge) {
+        maxAge = m.age;
+        maxAgeName = m.name;
+      }
+      if (m.age < minAge) {
+        minAge = m.age;
+        minAgeName = m.name;
+      }
+      sumAge += m.age / 1000;
+      ages.push(m.age / 1000 / 31536000);
+    }
+    if (m.idnumber && m.idnumber.length === 18) {
+      const birth = m.idnumber.slice(6, 14);
+      const monthDay = birth.slice(4);
+
+      if (!byMonthDay[monthDay]) byMonthDay[monthDay] = [];
+
+      byMonthDay[monthDay].push(m.name);
+    }
+  });
+
+  let maxMonthDayCount = 0;
+  let maxMonthDay = '';
+
+  Object.entries(byMonthDay).forEach(([md, names]) => {
+    if (names.length > maxMonthDayCount) {
+      maxMonthDayCount = names.length;
+      maxMonthDay = md;
+    }
+  });
+
+  const maxMonth = parseInt(maxMonthDay.slice(0, 2), 10);
+  const maxDay = parseInt(maxMonthDay.slice(2), 10);
+
+  return {
+    average: (sumAge / members.length / 31536000).toFixed(2),
+    min: (minAge / 1000 / 31536000).toFixed(2),
+    minName: minAgeName,
+    max: (maxAge / 1000 / 31536000).toFixed(2),
+    maxName: maxAgeName,
+    ages,
+    hist: getHistogramData(ages, 20),
+    maxMonthDay: `${maxMonth} 月 ${maxDay} 日`,
+    maxMonthDayCount,
+  };
+};
+
+export const getAddressInfo = (members, level = 'province') => {
+  const data = {};
+  members.forEach((m) => {
+    const { idnumber } = m;
+    if (idnumber) {
+      const cityCode = `${idnumber.substring(0, 4)}00`;
+      const provCode = `${idnumber.substring(0, 2)}0000`;
+      const code = level === 'city' ? cityCode : provCode;
+      if (code in data) {
+        data[code] += 1;
+      } else {
+        data[code] = 1;
+      }
+    }
+  });
+  const codeMap = {
+    110000: '北京市',
+    120000: '天津市',
+    130000: '河北省',
+    140000: '山西省',
+    150000: '内蒙古自治区',
+    210000: '辽宁省',
+    220000: '吉林省',
+    230000: '黑龙江省',
+    310000: '上海市',
+    320000: '江苏省',
+    330000: '浙江省',
+    340000: '安徽省',
+    350000: '福建省',
+    360000: '江西省',
+    370000: '山东省',
+    410000: '河南省',
+    420000: '湖北省',
+    430000: '湖南省',
+    440000: '广东省',
+    450000: '广西壮族自治区',
+    460000: '海南省',
+    500000: '重庆市',
+    510000: '四川省',
+    520000: '贵州省',
+    530000: '云南省',
+    540000: '西藏自治区',
+    610000: '陕西省',
+    620000: '甘肃省',
+    630000: '青海省',
+    640000: '宁夏回族自治区',
+    650000: '新疆维吾尔自治区',
+    710000: '台湾省',
+    810000: '香港特别行政区',
+    820000: '澳门特别行政区',
+  };
+  const ret = [];
+  Object.entries(data).forEach(([name, value]) => {
+    ret.push({ name: codeMap[name], value });
+  });
+  return ret;
+};
+
+export const getNameInfo = (members) => {
+  const fuxingList = ['欧阳', '上官', '皇甫', '令狐', '诸葛', '司徒', '司马', '申屠', '夏侯', '贺兰', '完颜', '慕容', '尉迟', '长孙'];
+  const surnameMap = {};
+  const nameMap = {};
+  members.forEach(({ name }) => {
+    if (name && name.length >= 2) {
+      if (name in nameMap) {
+        nameMap[name] += 1;
+      } else {
+        nameMap[name] = 1;
+      }
+
+      const fuxingTest = name.slice(0, 2);
+      if (fuxingList.includes(fuxingTest)) {
+        if (fuxingTest in surnameMap) {
+          surnameMap[fuxingTest] += 1;
+        } else {
+          surnameMap[fuxingTest] = 1;
+        }
+      } else {
+        const surname = name.slice(0, 1);
+        if (surname in surnameMap) {
+          surnameMap[surname] += 1;
+        } else {
+          surnameMap[surname] = 1;
+        }
+      }
+    }
+  });
+
+  let maxNameCount = 0;
+  let maxName = '';
+  Object.entries(nameMap).forEach(([name, value]) => {
+    if (value > maxNameCount) {
+      maxNameCount = value;
+      maxName = name;
+    }
+  });
+
+  const surnameData = [];
+  let maxSurnameCount = 0;
+  let maxSurname = '';
+  Object.entries(surnameMap).forEach(([name, value]) => {
+    surnameData.push({ name, value });
+    if (value > maxSurnameCount) {
+      maxSurnameCount = value;
+      maxSurname = name;
+    }
+  });
+
+  return {
+    maxName,
+    maxNameCount,
+    maxSurname,
+    maxSurnameCount,
+    surnameData: surnameData.sort((a, b) => b.value - a.value),
+  };
+};
